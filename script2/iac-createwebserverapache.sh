b683eecac884fc8db161e0a9ad30cf1cbc2b2d24
giuliacarlini <giuliacarlini@gmail.com>
2024-10-09 15:16:17 +0000

    Ajuste de script

--- script2/iac-createwebserverapache.sh
+++ script2/iac-createwebserverapache.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-apt update
-
-apt upgrade -y
-
-apt install apache2
-
-apt install unzip
-=======
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
->>>>>>> 5a38e20 (Ajuste de script)
 
 cd /tmp
 
@@ -21,18 +11,6 @@ wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.z
 
 unzip main.zip
 
-<<<<<<< HEAD
-cp /tmp/linux-site-dio-main/* /var/www/html/
-
-cd /scripts/
-
-git add iac-createwebserverapache.sh
-git commit -m "IaC Create Web Server Apache Script Bash"
-git push
-=======
 cd /tmp/linux-site-dio-main
 cp -R * /var/www/html/
 
-
-
->>>>>>> 5a38e20 (Ajuste de script)
